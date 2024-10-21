0f572d0f3437849692cf75a797ae74b22d5207b8
Maryana Jammal <maj45@mail.edu.lb>
2024-10-21 22:06:38 +0100

    just testing if this message shows

--- test.c
+++ test.c
@@ -1,20 +1,2 @@
-#include <stdio.h>
+//just testing file 
 
-int main() {
-    int num1, num2, sum;
-
-    // Asking for input
-    printf("Enter first number: ");
-    scanf("%d", &num1);
-
-    printf("Enter second number: ");
-    scanf("%d", &num2);
-
-    // Calculating the sum
-    sum = num1 + num2;
-
-    // Displaying the result
-    printf("The sum of %d and %d is: %d\n", num1, num2, sum);
-
-    return 0;
-}
