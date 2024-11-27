5d99b6a46c204866ee4bd35e4826d344b08d3dc1
Dani Makarem <dmakarem33@gmail.com>
2024-11-27 17:50:21 +0300

    Include checkShipSunk and checkWin functions

--- Phase 2/shipStatus.c
+++ Phase 2/shipStatus.c
@@ -1,1 +1,44 @@
+const char *checkShipSunk(char board[GRID_SIZE][GRID_SIZE], char shipChar)
+{
+    for (int i = 0; i < GRID_SIZE; i++)
+    {
+        for (int j = 0; j < GRID_SIZE; j++)
+        {
+            if (board[i][j] == shipChar)
+            {
+                return NULL;
+            }
+        }
+    }
 
+    switch (shipChar)
+    {
+    case 'C':
+        return "Carrier";
+    case 'B':
+        return "Battleship";
+    case 'D':
+        return "Destroyer";
+    case 'S':
+        return "Submarine";
+    default:
+        return NULL;
+    }
+}
+
+// Function to check if all ships on the board have been sunk
+int checkWin(char board[GRID_SIZE][GRID_SIZE])
+{
+    for (int i = 0; i < GRID_SIZE; i++)
+    {
+        for (int j = 0; j < GRID_SIZE; j++)
+        {
+            // Check if any ships remain on the board
+            if (board[i][j] != '~' && board[i][j] != '*' && board[i][j] != 'o')
+            {
+                return 0; // Game is not over
+            }
+        }
+    }
+    return 1; // All ships have been sunk
+}
